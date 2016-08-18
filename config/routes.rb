b77d45d8b9b7898b700d773e32ec0ca68ed7ffaf
b02611002 <b02611002@ntu.edu.tw>
2016-08-18 12:02:13 +0800

    removed Home/index

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,5 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'home/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
