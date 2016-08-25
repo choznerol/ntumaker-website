f367a7425a9dce3bc4362fdbc2371e3973fcaad0
b02611002 <b02611002@ntu.edu.tw>
2016-08-25 13:53:32 +0800

    new helper : full_title()

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,6 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
