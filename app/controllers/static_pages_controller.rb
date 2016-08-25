19425177144054d41ea5e727f2672d6eac7f0cfe
b02611002 <b02611002@ntu.edu.tw>
2016-08-25 11:53:45 +0800

    update page titles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,19 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
+  	# knowledge: 
+  	# In plain Ruby, these methods would simply do nothing. In Rails,
+  	# the situation is different—StaticPagesController is a Ruby 
+  	# class, but because it inherits from ApplicationController 
+  	# the behavior of its methods is specific to Rails: when visiting 
+  	# the URL /static_pages/home, Rails looks in the Static Pages 
+  	# controller and executes the code in the home action, and then 
+  	# renders the view (the V in MVC from Section 1.3.3) corresponding 
+  	# to the action. In the present case, the home action is empty, so 
+  	# all visiting /static_pages/home does is render the view.
   end
+
+  def facebook
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   
-  get 'static_pages/home'
-
-  # TODO:
-  # get 'about' => 'static_pages#about'
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
+  root 'static_pages#home'
 
   resources :posts
 
-  root 'static_pages#home'
+  get 'static_pages/home'
+  # TODO:
+  # get 'about' => 'static_pages#about'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+  get 'static_pages/facebook'
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
